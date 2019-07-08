c2fa8ae3becc8d4048a2fd9b6ce49fbfc6ee9119
woosungchu <woosungchu@gmail.com>
2019-07-08 10:07:56 +0900

    remove trash

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
 require('./activex');
 
-var path = require('path');
-var data_path = path.join(__dirname, '../data/');
-var filename = "persons.dbf";
-var constr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + data_path + ";Extended Properties=\"DBASE IV;\"";
-
-console.log("==> Preapre directory and delete DBF file on exists");
-var fso = new ActiveXObject("Scripting.FileSystemObject");
-if (!fso.FolderExists(data_path)) fso.CreateFolder(data_path);
-if (fso.FileExists(data_path + filename)) fso.DeleteFile(data_path + filename);
-
-console.log("==> Open connection");
-
-
-//----------------------------------------------
-
-
 var hwp = new ActiveXObject('HWPFrame.HwpObject.1');
 hwp.HAction.GetDefault('InsertText', hwp.HParameterSet.HInsertText.HSet);	// 텍스트 입력
 hwp.HParameterSet.HInsertText.Text = '테스트입니다.';
