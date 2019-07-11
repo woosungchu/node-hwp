082c2f7dc7c8b70242602603240a6f3bae21e67a
woosungchu <woosungchu@gmail.com>
2019-07-11 09:29:39 +0900

    complete automation

--- index.js
+++ index.js
@@ -3,28 +3,33 @@ var fn = require('./fn.js');
 
 var hwp = new ActiveXObject('HWPFrame.HwpObject.1');
 var hAct = hwp.HAction;
-var filename = (function(){return Date.now()})();
+var filename = Date.now();
+var students = [{name:'기창빈',grade:'가'},{name:'한상욱',grade:'다'}];
 
 hwp.RegisterModule("FilePathCheckDLL", "FilePathCheckerModule");
-hwp.Open("C:/aProjects/node-hwp/resource/template1.hwp","HWP","template:true"); // 접근제한
 
+for (var i = 0; i < students.length; i++) {
+  console.log(students[i].name,students[i].grade)
+  filename = Date.now() + '_' + students[i].name + '_'+ students[i].grade;
 
-// 내용 편집 시작
+  hwp.Open("C:/aProjects/node-hwp/resource/template1.hwp","HWP","template:true");
 
-hAct.Run("MoveDocBegin");
-fn.write(hwp,"진로와직업");
-hAct.Run("MoveDown");
-hAct.Run("MoveDown");
-fn.write(hwp," 2019학년도 1학기                   고등학교  1학년  1반  1번  이름 : 한 상 욱");
+  // 내용 편집 시작
 
-hAct.Run("MoveDocEnd");
-hAct.Run("MoveUp");
-hAct.Run("MoveUp");
-hAct.Run("MoveRight");
-hAct.Run("MoveRight");
-fn.write(hwp,"가나다 수준에 따른 평가");
+  hAct.Run("MoveDocBegin");
+  fn.write(hwp,"진로와직업");
+  hAct.Run("MoveDown");
+  hAct.Run("MoveDown");
+  fn.write(hwp," 2019학년도 1학기                   고등학교  1학년  1반  1번  이름 : 한 상 욱");
 
-// 내용 편집 끝
+  hAct.Run("MoveDocEnd");
+  hAct.Run("MoveUp");
+  hAct.Run("MoveUp");
+  hAct.Run("MoveRight");
+  hAct.Run("MoveRight");
+  fn.write(hwp,students[i].name + "은" + students[i].grade + '수준 입니다.');
 
+  // 내용 편집 끝
 
-hwp.SaveAs("C:/Users/admin/Downloads/test"+filename+".hwp","HWP","download:true");
+  hwp.SaveAs("C:/Users/admin/Downloads/test"+filename+".hwp","HWP","download:true");
+}
